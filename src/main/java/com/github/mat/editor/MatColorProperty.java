fd0abf9e9c135dc8f98746c8a1da8587f918a1d5
capdevon <jefferydeaver2010@gmail.com>
2024-02-19 12:04:42 +0100

    MatColorProperty: use AWT's JColorChooser.

--- src/main/java/com/github/mat/editor/MatColorProperty.java
+++ src/main/java/com/github/mat/editor/MatColorProperty.java
@@ -1,14 +1,24 @@
 package com.github.mat.editor;
 
+import java.awt.Color;
+
+import javax.swing.JColorChooser;
+import javax.swing.SwingUtilities;
+
+import com.github.tools.ColorUtils;
+import com.jme3.app.Application;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.simsilica.lemur.Axis;
+import com.simsilica.lemur.Button;
 import com.simsilica.lemur.Container;
 import com.simsilica.lemur.FillMode;
+import com.simsilica.lemur.GuiGlobals;
+import com.simsilica.lemur.HAlignment;
+import com.simsilica.lemur.Label;
 import com.simsilica.lemur.Panel;
-import com.simsilica.lemur.RollupPanel;
+import com.simsilica.lemur.component.IconComponent;
 import com.simsilica.lemur.component.SpringGridLayout;
-import com.simsilica.lemur.props.PropertyPanel;
 
 /**
  * 
@@ -16,81 +26,49 @@ import com.simsilica.lemur.props.PropertyPanel;
  */
 public class MatColorProperty extends MatParamProperty<ColorRGBA> implements MatPropertyBuilder {
 
-    private float red, green, blue, alpha;
-    private ColorRGBA color;
-
     public MatColorProperty(String name, Material material) {
         super(name, material);
-        this.color = getValue();
-        this.red = color.r;
-        this.green = color.g;
-        this.blue = color.b;
-        this.alpha = color.a;
     }
 
     @Override
     public Panel buildPanel() {
-        Container container = new Container(new SpringGridLayout(Axis.X, Axis.Y, FillMode.ForcedEven, FillMode.Even));
-        // container.addChild(new Label(name));
+        Container container = new Container(new SpringGridLayout(Axis.X, Axis.Y, FillMode.First, FillMode.Even));
+        container.addChild(new Label(this.getName() + ":"));
 
-        PropertyPanel propertyPanel = container.addChild(new PropertyPanel("glass"));
-        propertyPanel.addFloatProperty("R", this, "red", 0, 1, 0.01f);
-        propertyPanel.addFloatProperty("G", this, "green", 0, 1, 0.01f);
-        propertyPanel.addFloatProperty("B", this, "blue", 0, 1, 0.01f);
-        propertyPanel.addFloatProperty("A", this, "alpha", 0, 1, 0.01f);
+        // Color selection button
+        Button button = container.addChild(new Button("Change..."));
+        IconComponent icon = new IconComponent("Icons/color-plus.png");
+        icon.setHAlignment(HAlignment.Right);
+        button.setIcon(icon);
+        
+        Color initialColor = ColorUtils.toColorAWT(this.getValue());
+        button.addClickCommands(cmd -> {
+            
+            // invoke on AWT Thread
+            SwingUtilities.invokeLater(() -> {
+                Color selectedColor = JColorChooser.showDialog(null, "Choose Color...", initialColor, true);
+                if (selectedColor != null) {
+                    ColorRGBA jmeColor = ColorUtils.toColorRGBA(selectedColor);
+                    
+                    // set value from the JME Thread
+                    getApplication().enqueue(() -> {
+                        this.setValue(jmeColor);
+                    });
+                }
+            });
+        });
 
-//      Button update = container.addChild(new Button("Set"));
-//      update.addClickCommands(cmd -> {
-//          material.setColor(name, color);
-//      });
-
-        RollupPanel rollup = new RollupPanel(this.getName(), propertyPanel, "glass");
-        // rollup.setAlpha(0, false);
-        rollup.setOpen(false);
-        container.addChild(rollup);
-
-        return rollup;
+        return container;
     }
-
-    public float getRed() {
-        return red;
-    }
-
-    public void setRed(float red) {
-        this.red = red;
-        updateColor();
-    }
-
-    public float getGreen() {
-        return green;
-    }
-
-    public void setGreen(float green) {
-        this.green = green;
-        updateColor();
-    }
-
-    public float getBlue() {
-        return blue;
-    }
-
-    public void setBlue(float blue) {
-        this.blue = blue;
-        updateColor();
-    }
-
-    public float getAlpha() {
-        return alpha;
-    }
-
-    public void setAlpha(float alpha) {
-        this.alpha = alpha;
-        updateColor();
-    }
-
-    private void updateColor() {
-        color.set(red, green, blue, alpha);
-        setValue(color);
+    
+    /**
+     * Trick to access the Application through the GuiGlobals 
+     * which has no getter method for direct access.
+     * 
+     * @return
+     */
+    private Application getApplication() {
+        return GuiGlobals.getInstance().getPopupState().getApplication();
     }
 
 }
